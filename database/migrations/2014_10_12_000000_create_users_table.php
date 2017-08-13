77933aa23566f1094ff0dcac8d0629dbde4b8b9e
hxor <4kiddie04@gmail.com>
2017-08-13 21:05:21 +0700

    move User model to Models dir & add photo,level columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('level')->unsigned()->default(2);
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
